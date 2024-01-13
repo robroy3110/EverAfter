1c90b7d1eea42f6b6465e81595206d82471d6f37
Beatriz Rosa <beatrizarosa4@gmail.com>
2024-01-13 22:18:14 +0000

    hotfix was creating a song for each Everafter run and click ...

--- app/src/main/java/cm/everafter/EverAfter.kt
+++ app/src/main/java/cm/everafter/EverAfter.kt
@@ -122,7 +122,7 @@ fun EverAfter() {
             }
         }
     }
-    initializeSongs()
+
 }
 
 @Composable
--- app/src/main/java/cm/everafter/MainActivity.kt
+++ app/src/main/java/cm/everafter/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import cm.everafter.screens.playlist.initializeSongs
 import cm.everafter.ui.theme.EverAfterTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    initializeSongs()
                     EverAfter()
                 }
             }
