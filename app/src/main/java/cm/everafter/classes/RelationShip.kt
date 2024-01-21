15ae20ba4c362d5d0d9150f39f88c3de59c3b158
Ines-a22001936 <inesou02@hotmail.com>
2024-01-21 22:13:40 +0000

    notificacoes jogos diarias a funcionar

--- app/src/main/java/cm/everafter/classes/RelationShip.kt
+++ app/src/main/java/cm/everafter/classes/RelationShip.kt
@@ -1,3 +1,3 @@
 package cm.everafter.classes
 
-data class RelationShip(val pointsTotal: Int = 0,val pointsGames: Int= 0, val pointsMusic: Int = 0,val pointsDate: Int = 0,val pointsPictures: Int = 0, val date: String = "0-0-0", val user1: String = "", val user2: String = "")
+data class RelationShip(val pointsTotal: Int = 0,val pointsGames: Int= 0, val pointsMusic: Int = 0,val pointsDate: Int = 0,val pointsPictures: Int = 0, val date: String = "0-0-0", val user1: String = "", val user2: String = "", val favgames: List<Game>)
